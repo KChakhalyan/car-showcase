beac0e3c1b79112aa1fff85e1582951a0d4ea8f0
KChakhalyan <karenchakhalyan23@gmail.com>
2023-09-22 23:48:27 +0400

    almost done with applicaition

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -6,7 +6,7 @@ import { Combobox, Transition } from "@headlessui/react";
 import { manufacturers } from "../constants";
 import { SearchManuFacturerProps } from "@/types";
 
-const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManuFacturerProps) => {
+const SearchManufacturer = ({ selected, setSelected }: SearchManuFacturerProps) => {
    const [query, setQuery] = useState("");
 
    const filteredManufacturers =
@@ -21,7 +21,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManuFacture
 
    return (
       <div className="search-manufacturer">
-         <Combobox value={manufacturer} onChange={setManufacturer}>
+         <Combobox value={selected} onChange={setSelected}>
             <div className="relative w-full">
                {/* Button for the combobox. Click on the icon to see the complete dropdown */}
                <Combobox.Button className="absolute top-[14px]">
--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,29 +1,28 @@
 "use client";
+
 import { ShowMoreProps } from "@/types";
-import { useRouter } from "next/navigation";
-import { CustomButton } from ".";
-import { updatedSearchParams } from "@/utils";
+import { CustomButton } from "@/components";
 
-const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
-   const router = useRouter();
-
+const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
    const handleNavigation = () => {
+      // Calculate the new limit based on the page number and navigation type
       const newLimit = (pageNumber + 1) * 10;
-      const newPathName = updatedSearchParams("limit", `${newLimit}`);
-      router.push(newPathName);
+
+      setLimit(newLimit);
    };
 
    return (
       <div className="w-full flex-center gap-5 mt-10">
          {!isNext && (
             <CustomButton
-               title="Show more"
                btnType="button"
-               containerStyles="bg-primary-blue text-white rounded-full"
+               title="Show More"
+               containerStyles="bg-primary-blue rounded-full text-white"
                handleClick={handleNavigation}
             />
          )}
       </div>
    );
 };
+
 export default ShowMore;
